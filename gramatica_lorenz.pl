5230b554b6ea5743ee323de2d323c2777fb55d29
Marion <Mundos Graves@.(none)>
2010-02-10 21:14:17 -0400

    subiendo

--- gramatica_lorenz.pl
+++ gramatica_lorenz.pl
@@ -1,6 +1,9 @@
+% Preguntas permitidas: quien, quienes 
 pronom(singular) --> [quien].
 pronom(plural) --> [quienes].
 es_verdad --> [es],[verdad],[que].
+
+% Verbos permitidos: es, son.
 verbo(singular) --> [es].
 verbo(plural) --> [son].
 articulo(singular,femenino) --> [la].
@@ -8,7 +11,8 @@ articulo(plural,femenino) --> [las].
 articulo(singular,masculino) --> [el].
 articulo(plural,masculino) --> [los].
 
-
+%Relaciones singulares:
+%Femeninas.
 relacion(singular,femenino,X,Y) --> [esposa], {esposa(Y,X)}.
 relacion(singular,femenino,X,Y) --> [abuela],{abuela(Y,X)}.
 relacion(singular,femenino,X,Y) --> [madre],{madre(Y,X)}.
@@ -20,14 +24,7 @@ relacion(singular,femenino,X,Y) --> [sobrina],{sobrina(Y,X)}.
 relacion(singular,femenino,X,Y) --> [suegra],{suegra(Y,X)}.
 relacion(singular,femenino,X,Y) --> [cunada],{cunada(Y,X)}.
 
-relacion(plural,femenino,X,Y) --> [abuelas],{abuelas(Y,X)}.
-relacion(plural,femenino,X,Y) --> [hijas],{hijas(Y,X)}.
-relacion(plural,femenino,X,Y) --> [nietas],{nietas(Y,X)}.
-relacion(plural,femenino,X,Y) --> [hermanas],{hermanas(Y,X)}.
-relacion(plural,femenino,X,Y) --> [tias],{tias(Y,X)}.
-relacion(plural,femenino,X,Y) --> [sobrinas],{sobrinas(Y,X)}.
-relacion(plural,femenino,X,Y) --> [cunadas],{cunadas(Y,X)}.
-
+%Masculinas.
 relacion(singular,masculino,X,Y) --> [esposo],{esposo(Y,X)}.
 relacion(singular,masculino,X,Y) --> [abuelo],{abuelo(Y,X)}.
 relacion(singular,masculino,X,Y) --> [padre],{padre(Y,X)}.
@@ -39,6 +36,18 @@ relacion(singular,masculino,X,Y) --> [sobrino],{sobrino(Y,X)}.
 relacion(singular,masculino,X,Y) --> [suegro],{suegro(Y,X)}.
 relacion(singular,masculino,X,Y) --> [cunado],{cunado(Y,X)}.
 
+%Relaciones Plurales:
+
+% Femeninas.
+relacion(plural,femenino,X,Y) --> [abuelas],{abuelas(Y,X)}.
+relacion(plural,femenino,X,Y) --> [hijas],{hijas(Y,X)}.
+relacion(plural,femenino,X,Y) --> [nietas],{nietas(Y,X)}.
+relacion(plural,femenino,X,Y) --> [hermanas],{hermanas(Y,X)}.
+relacion(plural,femenino,X,Y) --> [tias],{tias(Y,X)}.
+relacion(plural,femenino,X,Y) --> [sobrinas],{sobrinas(Y,X)}.
+relacion(plural,femenino,X,Y) --> [cunadas],{cunadas(Y,X)}.
+
+% Masculinos.
 relacion(plural,masculino,X,Y) --> [abuelos],{abuelos(Y,X)}.
 relacion(plural,masculino,X,Y) --> [hijos],{hijos(Y,X)}.
 relacion(plural,masculino,X,Y) --> [nietos],{nietos(Y,X)}.
@@ -47,6 +56,7 @@ relacion(plural,masculino,X,Y) --> [tios],{tios(Y,X)}.
 relacion(plural,masculino,X,Y) --> [sobrinos],{sobrinos(Y,X)}.
 relacion(plural,masculino,X,Y) --> [cunados],{cunados(Y,X)}.
 
+% Preposiciones permitidas: de la, de las, del, de los.
 prepos_art(singular,femenino) --> [de],[la].
 prepos_art(plural,femenino) --> [de],[las].
 prepos_art(singular,masculino) --> [del].
